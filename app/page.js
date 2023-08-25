f4bc517e44ec6ad45840d9e4259bf7e5f7ebc366
Shay Kelley <smkelley91@gmail.com>
2023-08-25 02:39:56 -0500

    added api route and have a post request working

--- app/page.js
+++ app/page.js
@@ -4,9 +4,20 @@ import styles from './page.module.css'
 
 export default function Home() {
   const [input, setInput] = useState('')
-  const handleSubmit = (e) => {
+  const [result, setResult] = useState({})
+  async function handleSubmit(e){
     e.preventDefault()
     console.log(input)
+    const response = await fetch('/api/weather', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({input})
+    })
+    const data = await response.json()
+    console.log(data)
+    setResult(data)
     setInput('')
   }
   return (
