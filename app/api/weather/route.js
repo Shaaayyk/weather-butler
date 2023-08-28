bfc16917b66d33e2d34906cbc17de3f2ee4e9e89
Shay Kelley <smkelley91@gmail.com>
2023-08-28 00:01:42 -0500

    starting to incorperate openai to suggest clothing based upon the weather

--- app/api/weather/route.js
+++ app/api/weather/route.js
@@ -1,4 +1,23 @@
 import { NextResponse } from 'next/server'
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const chatMessages = [
+  {
+    role: 'system',
+    content: 'You are a helpful assistant'
+  },
+  {
+    role: 'user',
+    content: ''
+  }
+]
+
 
 export async function POST(request) {
   const geoURL = 'http://api.openweathermap.org/geo/1.0/direct?q='
@@ -14,6 +33,19 @@ export async function POST(request) {
   const weatherResponse = await fetch(`${weatherURL}lat=${lat}&lon=${lon}&appid=${process.env.OPENWEATHER_API_KEY}`)
   const weatherData = await weatherResponse.json()
   console.log(weatherData)
+
+  const date = new Date(weatherData.dt * 1000)
+  console.log(date.toString())
+
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [],
+    // {role: 'user', content: 'message'}
+    temperature: 0.7,
+  });
+  // console.log(completion.data.choices[0].message)
+
+
   return NextResponse.json({message: `You sent in ${body.input}`, weather: weatherData})
 }
 
