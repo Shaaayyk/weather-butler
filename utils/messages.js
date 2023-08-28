9648da0c61427098809226064c75f79bfa8d1939
Shay Kelley <smkelley91@gmail.com>
2023-08-28 02:07:11 -0500

    continuing modularization for api functions

--- utils/messages.js
+++ utils/messages.js
@@ -6,11 +6,13 @@ const openai = new OpenAI({
 
 const chatMessages = []
 
-function initChatMessages(geoObj, weatherObj) {
+export async function initChatMessages(geoObj, weatherObj) {
   const system = {
     role: 'system',
     content: 'You are a helpful assistant'
   }
+
+  chatMessages.push(system)
   
   const date = new Date(weatherObj.dt * 1000)
   const city = geoObj[0].name
@@ -29,12 +31,34 @@ function initChatMessages(geoObj, weatherObj) {
         ${temperature} degrees in Fahrenheit. The humidity is ${humidity} 
         percent. The wind speed is ${windSpeed} mph. Also what colors should I wear?
       `
-    }
+  }
   
+  chatMessages.push(user)
+  const completion = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: chatMessages,
+    temperature: 0.7,
+  });
+  chatMessages.push(completion.choices[0].message)
+  return chatMessages
 }
 
-const completion = await openai.chat.completions.create({
-  model: "gpt-3.5-turbo",
-  messages: chatMessages,
-  temperature: 0.7,
-});
+export async function getCompletion(newMessage, _messages) {
+  try {
+    const user = {
+      role: 'user',
+      content: newMessage
+    }
+    chatMessages.push(user)
+    const completion = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: chatMessages,
+      temperature: 0.7,
+    });
+    chatMessages.push(completion.choices[0].message)
+    return chatMessages
+  } catch (error) {
+    console.log(error)
+    return error
+  }
+}
